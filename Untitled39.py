eabd08f05581841b55ee11160e1e9faa236dcd13
SharpsBrain <109470818+SharpsBrain@users.noreply.github.com>
2022-09-13 09:48:54 +0500

    Add files via upload

---
+++ Untitled39.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+mylist=["apple", "banana", "cherry", "apple", "banana", "cherry"]
+print(len(mylist))
+print(mylist)
+
+
+# In[6]:
+
+
+list1=["apple","banana","cherry"]
+list2=[1,2,3,5,6,7]
+list3=[True, False, True]
+print(list1,list2,list3)
+
+
+# In[8]:
+
+
+list1=["abc", 34 , True, 40,"male"]
+print(list1)
+
+
+# In[9]:
+
+
+#type():
+list1=[1,3,4,5]
+print(type(list1))
+
+
+# In[10]:
+
+
+thislist=list(("apple", "banana", "cherry"))#note that double round brackets 
+print(thislist)
+
+
+# In[ ]:
+
+
+
+
